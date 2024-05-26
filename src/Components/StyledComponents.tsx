2800fb6ad95ba678aa88e9024c824032ac405c68
TheJB4 <juanmer382@gmail.com>
2024-05-26 14:53:20 -0300

    iniciando login

---
+++ src/Components/StyledComponents.tsx
@@ -0,0 +1,5 @@
+import { styled } from "nativewind";
+import { View,Text } from "react-native";
+
+export const StyledView = styled(View)
+export const StyledText = styled(Text)
---
+++ src/Views/Login.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { StyledText, StyledView } from "../Components/StyledComponents";
+
+export default function Login(){
+    return(
+        <StyledView>
+            <StyledText className="text-lg">Login</StyledText>
+        </StyledView>
+    )
+}
